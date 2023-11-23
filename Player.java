30f41248d4d5e1acf88f702b4422a075929284fd
roslynpius <roslyn.pius@gmail.com>
2023-11-23 23:22:56 +0530

    Implemented USECASE 2: single player rolls dice

--- Player.java
+++ Player.java
@@ -1,9 +1,16 @@
 package com.bridgelabz.Snakes_And_Ladders;
+import java.util.Random;
 
 public class Player {
     int position;
 
     Player() {
         position = 0;
+    }
+    public int rollDice(){
+        Random random = new Random();
+
+        int randomValue = random.nextInt(6) + 1;
+        return randomValue;
     }
 }
--- SnakesAndLadder.java
+++ SnakesAndLadder.java
@@ -3,8 +3,26 @@ package com.bridgelabz.Snakes_And_Ladders;
 public class SnakesAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game!");
+
         Player p1=new Player();
         System.out.println("Start Position:"+p1.position);
+        System.out.println("Roll the Dice!");
+        rollDice(p1);
+        
+    }
+
+    /**
+     * Player rolls dice and the position is updated
+     * 
+     * @param p - Player Object.
+     */
+    public static void rollDice(Player p){
+        int diceValue=p.rollDice();
+
+        p.position+=diceValue;
+        
+        System.out.print("Dice Value: "+diceValue+"   ");        
+        System.out.println("Current Position: "+p.position);
     }
     
 }
