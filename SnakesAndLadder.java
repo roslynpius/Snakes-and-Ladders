3c253d0db53d4cecf188bc7592e2d98dc9e05f1e
roslynpius <roslyn.pius@gmail.com>
2023-11-23 21:29:33 +0530

    Implemented USECASE 1: single player at start position 0

--- SnakesAndLadder.java
+++ SnakesAndLadder.java
@@ -3,6 +3,8 @@ package com.bridgelabz.Snakes_And_Ladders;
 public class SnakesAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game!");
+        Player p1=new Player();
+        System.out.println("Current Position:"+p1.position);
     }
     
 }
