02a68cd4c3c87ed69a2b0080f4ac9ad6fa50951c
roslynpius <roslyn.pius@gmail.com>
2023-11-23 21:19:54 +0530

    Initial commit

---
+++ SnakesAndLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.Snakes_And_Ladders;
+
+public class SnakesAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game!");
+    }
+    
+}
